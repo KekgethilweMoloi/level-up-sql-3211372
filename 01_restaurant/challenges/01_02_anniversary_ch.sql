095ff8890e8a82fc31b2bf12073504819378d3f2
KekgethilweKey <61177186+KekgethilweMoloi@users.noreply.github.com>
2023-12-04 10:56:38 +0000

    Create PartyAttendees table.

--- 01_restaurant/challenges/01_02_anniversary_ch.sql
+++ 01_restaurant/challenges/01_02_anniversary_ch.sql
@@ -1,7 +1,14 @@
--- Create a table in the database to store customer
--- responses to our anniversary invitation.
-
+/*
+CREATED BY: Kekgethilwe Moloi
+CREATE DATE: 04/12/2023
+DESCRIPTION: Create a table in the database to store customer responses to our anniversary invitation.
+*/
 -- Associate a customer's ID number with the number of people
 -- they plan to bring in their party.
 
 -- Hint: SQLite offers the INTEGER and REAL datatypes
+
+CREATE TABLE PartyAttendees(
+ CustomerID INTEGER,
+ PartySize INTEGER
+);
