f84e3dd3d7c87dc88dcfc4cc161a42a1362d8d68
KekgethilweKey <61177186+KekgethilweMoloi@users.noreply.github.com>
2023-12-11 19:27:56 +0000

    Find number of avaible books by subtracting number of books on loan from number of books

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,20 @@
--- Determine how many copies of the book 'Dracula'
--- are available for library patrons to borrow.
+/*
+CREATED BY: Kekgethilwe Moloi
+CREATE DATE: 11/12/2023
+DESCRIPTION: Determine how many copies of the book 'Dracula' 
+are available for library patrons to borrow.
+*/
+
+SELECT
+--Count how many copies the library has
+(
+SELECT COUNT(Title)
+FROM Books
+WHERE Books.Title='Dracula')-
+--Find how many books are still on loan
+(
+SELECT COUNT(Books.Title)
+FROM Books
+JOIN Loans
+ON Books.BookID=Loans.BookID
+WHERE Title='Dracula' AND Loans.ReturnedDate ISNULL) AS NumOfAvailableBooks
