70f33c796cfff181651d54a8c836574593c2cca0
KekgethilweKey <61177186+KekgethilweMoloi@users.noreply.github.com>
2023-12-12 18:49:36 +0000

    Create a list of patrons who have checked out the fewest books

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,12 @@
--- Prepare a report of the library patrons
--- who have checked out the fewest books.
+/*
+CREATED BY: Kekgethilwe Moloi
+CREATE DATE: 12/12/2023
+DESCRIPTION: Prepare a report of the library patrons who have checked out the fewest books.
+*/
+SELECT COUNT(LoanID) AS NumOfBooks, Patrons.FirstName, Patrons.Email
+FROM Loans
+JOIN Patrons
+ON Loans.PatronID = Patrons.PatronID
+GROUP BY Loans.PatronID
+ORDER BY NumOfBooks
+LIMIT 5
